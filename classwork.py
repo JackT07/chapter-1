6ac18e6fe1d3882d791f49364ccb194cbd282ac3
JackT07 <ctw20.jack.town@corketb.ie>
2024-09-13 13:29:42 +0100

    Add files via upload

---
+++ classwork.py
@@ -0,0 +1,28 @@
+age = 17
+print("what a great age!")
+
+#task 2
+num1 = 2.5
+num2 = 3.5
+answer = num1 + num2
+minusanswer = num1 - num2
+timesanswer = num1 * num2
+
+print(answer)
+print(minusanswer)
+print(timesanswer)
+
+num1 = 2.5
+num2 = 3.5
+average_answer = (num1 + num2) /2
+remainder_answer = num1 % num2
+power_answer = num1 ** num2
+
+print(average_answer)
+print(remainder_answer) 
+print(power_answer)
+
+fahrenheit = 87
+celcius = (5 / 9) * (fahrenheit - 32)
+rounded = round(celcius)
+print("The temperature of (fahrenheit) degrees Fahrenheit is approximately (rounded) Celsius")
---
+++ task 5 and 6.py
@@ -0,0 +1,19 @@
+# Homework Task 5 and 6
+distance_in_m = 7000
+m_to_km = 1.60935
+distance_in_km = round(distance_in_m * m_to_km)
+print("Distance from Ireland to Singapore:,7000km")
+cost_per_km = 900
+answer = (cost_per_km * distance_in_km)
+print(answer)
+print("\n")
+# 6
+h = float(int(6))
+r = float(int(2))
+pi = 3.14
+volume = pi * r ** 2 * h
+print(volume)
+totalliquid = float(int(348))
+need_cylinder = totalliquid // volume
+print(need_cylinder)
+
